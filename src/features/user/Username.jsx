128019359d430e008236d816aec1e5ecf867a400
Temo Avalishvili <temo.avalishvili7@gmail.com>
2024-06-26 16:46:19 +0400

    whole bunch of additions :D

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,12 @@
-function Username() {
-  return <div className="hidden text-sm font-semibold md:block">Temo</div>;
-}
+import { useSelector } from "react-redux";
 
+function Username() {
+  const username = useSelector((state) => state.user.username);
+
+  if (!username) return null;
+
+  return (
+    <div className="hidden text-sm font-semibold md:block">{username}</div>
+  );
+}
 export default Username;
